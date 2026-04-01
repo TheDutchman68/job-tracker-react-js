1a9b2f12e5505ba8f349d8d152a78af9bb1b9292
TheDutchman68 <nd527589@gmail.com>
2026-04-01 21:57:11 +0300

    add User model and test MongoDB insertion

---
+++ server/models/User.js
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please provide name'],
+    trim: true,
+    maxlength: [50, 'Name cannot be more than 50 characters'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Please provide email'],
+    unique: true,
+    match: [
+      /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/,
+      'Please provide a valid email',
+    ],
+  },
+  password: {
+    type: String,
+    required: [true, 'Please provide password'],
+    minlength: 6,
+  },
+}, { timestamps: true });
+
+export default mongoose.model('User', userSchema);
--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import User from './models/User.js'
+
 
 dotenv.config();
 
@@ -8,6 +10,23 @@ mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.log(err));
 
+const testUser = async () => {
+  try {
+    const user = await User.create({
+      name: 'Test User',
+      email: 'testuser@example.com',
+      password: '123456',
+    });
+    console.log('✅ User created:', user);
+  } catch(error){
+    console.log('❌ Error creating user:', error.message)
+  }
+}
+
+
+testUser();
+
+  
 const app = express();
 
 app.get('/', (req, res) => {
