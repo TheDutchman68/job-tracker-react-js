e6ba1b72489e30039d2cdced178d8aee751669d9
TheDutchman68 <nd527589@gmail.com>
2026-04-02 21:13:11 +0300

    Add CRUD routes and search/filter for jobs

--- server/routes/jobs.js
+++ server/routes/jobs.js
@@ -26,4 +26,67 @@ router.post('/', async (req, res) => {
   }
 });
 
+
+
+router.get('/:id', async (req,res) => {
+  try {
+    const { id: jobId} = req.params;
+
+    const job = await Job.findOne({ _id: jobId, createdBy: req.user.userId });
+    if (!job) return res.status(404).json({ success: false, message: 'Job not found' });
+
+    res.status(200).json({ success: true, data: job });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+})
+
+
+router.patch('/:id', async (req,res) => {
+  try {
+    const { id: jobId } = req.params;
+
+    const job = await Job.findOne({ _id: jobId, createdBy: req.user.userId});
+    if (!job) return res.status(404).json({success: false, message: 'Job not found'});
+
+    Object.assign(job, req.body);
+    await job.save();
+
+    res.status(200).json({success: true, data: job});
+  } catch(error){
+    res.status(400).json({success: false, message: error.message});
+  }
+});
+
+router.delete('/:id', async (req,res) => {
+  try {
+    const { id: jobId } = req.params;
+
+    const job = await Job.findOneAndDelete({ _id: jobId, createdBy: req.user.userId});
+    if (!job) return res.status(404).json({success: false, message: 'Job not found'});
+
+    res.status(200).json({ success: true, message: 'Job deleted' });
+  }catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+})
+
+router.get('/', async (req, res) => {
+  try {
+    const { search, status } = req.query;
+
+    let queryObject = { createdBy: req.user.userId };
+
+    if (search) queryObject.position = { $regex: search, $options: 'i' };
+    if (status) queryObject.status = status;
+
+    const jobs = await Job.find(queryObject);
+
+    res.status(200).json({ success: true, data: jobs });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+});
+
+
 export default router;
