0589bc304740c2cc5b518235f01197be38e2ed06
TheDutchman68 <nd527589@gmail.com>
2026-04-10 14:50:35 +0300

    test: setup Jest with ES modules support and add initial API test

--- server/server.js
+++ server/server.js
@@ -1,9 +1,6 @@
-import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import authRoutes from './routes/auth.js';
-import jobRoutes from './routes/jobs.js';
-import cors from 'cors';
+import app from './app.js';
 
 dotenv.config();
 
@@ -11,22 +8,8 @@ mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.log(err));
 
-  
-const app = express();
-
-app.use(express.json());
-app.use(cors());
-app.use('/api/users', authRoutes);
-app.use('/api/jobs', jobRoutes);
-
-app.get('/', (req, res) => {
-  res.send('Api is running');
-});
-
 const PORT = process.env.PORT || 5001;
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-
-
-
